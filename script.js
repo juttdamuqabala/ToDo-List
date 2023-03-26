d651fd19908712d2a3bf4d46fa61cdd091800d4c
zaki <gostudyzaki@gmail.com>
2023-03-26 22:18:53 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+let inputbox = document.getElementById('box');
+let listcontainer = document.getElementById('list-container');
+
+function addtask(){
+    if(inputbox.value === ''){
+        alert('You must write something!')
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputbox.value;
+        listcontainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputbox.value = "";
+    saveData();
+}
+listcontainer.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }   
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false)
+
+function saveData(){
+    localStorage.setItem("data",listcontainer.innerHTML);
+}
+
+showTask = ()=>{
+    listcontainer.innerHTML =  localStorage.getItem("data");
+}
+showTask(); 
